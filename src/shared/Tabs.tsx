9b097e44d7ddd471fdc90ac1dce23e05b9d01b3f
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-11-25 00:35:32 -0500

    tabs - current tab

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,7 +1,7 @@
 import { HomeTabPage } from "@/pages/HomeTabPage/HomeTabPage";
 import { SearchTabPage } from "@/pages/SearchTabPage/SearchTabPage";
 import { ProfileTabPage } from "@/pages/ProfileTabPage/ProfileTabPage";
-
+import "./Tabs.css";
 import {
   IonIcon,
   IonRouterOutlet,
@@ -13,13 +13,10 @@ import { Route } from "react-router";
 import homeIcon from "@/assets/tabs/home.svg";
 import searchIcon from "@/assets/tabs/search.svg";
 import personIcon from "@/assets/tabs/person.svg";
-import { useState } from "react";
 
 const Tabs: React.FC = () => {
-  const [selectedTab, setselectedTab] = useState("home");
-
   return (
-    <IonTabs onIonTabsWillChange={(tab) => setselectedTab(tab.detail.tab)}>
+    <IonTabs>
       <IonRouterOutlet>
         <Route exact path="/tabs/home" component={HomeTabPage}></Route>
         <Route exact path="/tabs/search" component={SearchTabPage}></Route>
@@ -27,42 +24,21 @@ const Tabs: React.FC = () => {
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
         <IonTabButton tab="home" href="/tabs/home">
-          <div
-            className={
-              "px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 " +
-              (selectedTab == "home" && "bg-primary-soft")
-            }
-          >
+          <div className="px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 bg-tab">
             <IonIcon aria-hidden="true" src={homeIcon} className="h-6 w-6" />
-            {selectedTab == "home" && (
-              <div className="text-xs font-medium">Home</div>
-            )}
+            <div className="text-xs font-medium tab-label">Home</div>
           </div>
         </IonTabButton>
         <IonTabButton tab="search" href="/tabs/search">
-          <div
-            className={
-              "px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 " +
-              (selectedTab == "search" && "bg-primary-soft")
-            }
-          >
+          <div className="px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 bg-tab">
             <IonIcon aria-hidden="true" src={searchIcon} className="h-6 w-6" />
-            {selectedTab == "search" && (
-              <div className="text-xs font-medium">Search</div>
-            )}
+            <div className="text-xs font-medium tab-label">Search</div>
           </div>
         </IonTabButton>
         <IonTabButton tab="profile" href="/tabs/profile">
-          <div
-            className={
-              "px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 " +
-              (selectedTab == "profile" && "bg-primary-soft")
-            }
-          >
+          <div className="px-3 py-2 rounded-2xl flex gap-1 items-center transition-all duration-300 bg-tab">
             <IonIcon aria-hidden="true" src={personIcon} className="h-6 w-6" />
-            {selectedTab == "profile" && (
-              <div className="text-xs font-medium">Profile</div>
-            )}
+            <div className="text-xs font-medium tab-label">Profile</div>
           </div>
         </IonTabButton>
       </IonTabBar>
